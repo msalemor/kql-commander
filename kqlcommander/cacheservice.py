550bcbe0d497bc3dd3bf5e2cbb2409b8d4d8ea21
Andre Dewes <andredewes@microsoft.com>
2024-10-30 21:47:15 -0300

    Fixing cache and duplicate page load

--- kqlcommander/cacheservice.py
+++ kqlcommander/cacheservice.py
@@ -20,19 +20,21 @@ lock = asyncio.Lock()
 async def write_cache(filename, cache) -> None:
     """Write the cache dictionary to disk asynchronously."""
     async with lock:
-        async with aiofiles.open(filename, 'w+b') as file:
-            pickle.dump(cache, file)
+        async with aiofiles.open(filename, 'wb') as file:
+             pickled_cache = pickle.dumps(cache)
+             await file.write(pickled_cache)
 
 
 async def read_cache(filename) -> bytes:
     """Read a cache dictionary from disk."""
     try:
-        async with aiofiles.open(filename, mode='r+b') as file:
-            cache = pickle.load(file)
+        async with aiofiles.open(filename, mode='rb') as file:
+            content = await file.read()
+        cache = pickle.loads(content)
+    except FileNotFoundError:
+        cache = {}
     except file_error as e:
-        if file_error is IOError:
-            if e.errno != ENOENT:
-                raise
+        print("Error reading cache:", e)
         cache = {}
     return cache
 
@@ -42,29 +44,29 @@ FILE_NAME = "./cache.dat"
 async def save_key(filename, key, value, ttl=3600) -> None:
     """Write a key:value pair to cache."""
     # ttl is "time to live" of the item in seconds
-    # cache = await read_cache(filename)
-    # expiry = int(time.time() + ttl)
-    # cache[key] = (expiry, value)
-    # await write_cache(filename, cache)
-    await asyncio.sleep(.01)
-    return None
+    cache = await read_cache(filename)
+    expiry = int(time.time() + ttl)
+    cache[key] = (expiry, value)
+    await write_cache(filename, cache)
+    #await asyncio.sleep(.01)
+    #return None
 
 
 async def load_key(filename, key) -> str | None:
     """Read the value for given key from cache."""
-    # cache = await read_cache(filename)
-    # try:
-    #     expiry, value = cache[key]
-    # except KeyError:
-    #     return None
-    # if time.time() > expiry:
-    #     # Expired key, delete it
-    #     del cache[key]
-    #     await write_cache(filename, cache)
-    #     return None
-    # return value
-    await asyncio.sleep(.01)
-    return None
+    cache = await read_cache(filename)
+    try:
+        expiry, value = cache[key]
+    except KeyError:
+        return None
+    if time.time() > expiry:
+        # Expired key, delete it
+        del cache[key]
+        await write_cache(filename, cache)
+        return None
+    return value
+    #await asyncio.sleep(.01)
+    #return None
 
 
 async def prune_cache(filename):
